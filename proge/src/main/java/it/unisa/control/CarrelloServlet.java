16e69a9f4ba26c0f1d157c489e37d9cb79ddda1c
FrancescoCasillo <162646898+FrancescoCasillo@users.noreply.github.com>
2024-07-17 18:29:02 +0200

    p

---
+++ proge/src/main/java/it/unisa/control/CarrelloServlet.java
@@ -0,0 +1,80 @@
+package it.unisa.control;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import it.unisa.model.Carrello;
+import it.unisa.model.ItemCarrello;
+import it.unisa.model.ProdottoBean;
+import it.unisa.model.ProdottoDao;
+
+@WebServlet("/carrello")
+public class CarrelloServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		ProdottoDao prodDao = new ProdottoDao();
+		ProdottoBean bean = new ProdottoBean();
+		Carrello cart = (Carrello)request.getSession().getAttribute("cart");
+		if(cart == null) {
+			cart = new Carrello();
+			request.getSession().setAttribute("cart", cart);
+		}
+		
+		String action = request.getParameter("action");
+		String quantita = request.getParameter("qnt");
+		String redirectedPage = request.getParameter("page");
+		
+		
+		try {
+			if (action != null) {
+				if (action.equalsIgnoreCase("addC")) {
+					int id = Integer.parseInt(request.getParameter("id"));
+					cart.addProdotto(prodDao.doRetrieveByKey(id));	
+				} else if (action.equalsIgnoreCase("deleteC")) {
+					int id = Integer.parseInt(request.getParameter("id"));
+					cart.deleteProdotto(prodDao.doRetrieveByKey(id));
+					
+				}
+			}
+			if(quantita!=null) {
+				int id = Integer.parseInt(request.getParameter("Id"));
+				ItemCarrello item = cart.getItem(id);
+				item.setQuantitaCarrello(Integer.parseInt(quantita));
+				
+			}
+
+			
+			
+		} catch (SQLException e) {
+			System.out.println("Error:" + e.getMessage());
+		}
+		
+	
+		
+		request.getSession().setAttribute("cart", cart);
+		request.setAttribute("cart", cart);
+		
+		
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/" + redirectedPage);
+			dispatcher.forward(request, response);
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		super.doPost(request, response);
+	}
+	
+
+}

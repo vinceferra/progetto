8a871f70dc73e64d1631bfd12f57123b6f7e1424
vinceferra <vincyferry97@gmail.com>
2026-07-23 01:02:55 +0200

    Fix bug searchbar

--- proge/src/main/WebContent/script/header.js
+++ proge/src/main/WebContent/script/header.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+	const contextPath = document.body.dataset.contextPath;
+	
             var $searchbar = $("#searchbar");
             var $risultati = $(".risultati");
 
